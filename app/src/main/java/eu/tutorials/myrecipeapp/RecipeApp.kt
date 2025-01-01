ef29dad27bf1df58fcd33b2df71ec7c6d3da21c3
dhirojmk <sahoodhiroj@gmail.com>
2025-01-01 21:08:47 +0530

    Initial commit

---
+++ app/src/main/java/eu/tutorials/myrecipeapp/RecipeApp.kt
@@ -0,0 +1,30 @@
+package eu.tutorials.myrecipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun RecipeApp(modifier: Modifier = Modifier, navController: NavHostController) {
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewstate by recipeViewModel.categoriesState
+    NavHost(navController = navController, startDestination = Screen.RecipeScreen.route) {
+        composable(Screen.RecipeScreen.route) {
+            RecipeScreen(viewstate = viewstate, navigateToDetail = {
+                navController.currentBackStackEntry?.savedStateHandle?.set("cat", it)
+                navController.navigate(Screen.DetailScreen.route)
+            })
+        }
+        composable(Screen.DetailScreen.route) {
+            val category =
+                navController.previousBackStackEntry?.savedStateHandle?.get<Category>("cat")
+                    ?: Category("", "", "", "")
+            CategoryDetailScreen(category = category)
+        }
+
+    }
+}
